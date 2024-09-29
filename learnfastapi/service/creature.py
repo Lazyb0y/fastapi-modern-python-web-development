d2dce30cd3d777350d45222475457581ded7de42
Foysal Ahamed <thisisfoysal@gmail.com>
2024-09-29 16:45:12 +0600

    Add service for creature and explorer

---
+++ learnfastapi/service/creature.py
@@ -0,0 +1,26 @@
+from learnfastapi.model.creature import Creature
+import learnfastapi.fake.creature as data
+
+
+def get_all() -> list[Creature]:
+    return data.get_all()
+
+
+def get_one(name: str) -> Creature | None:
+    return data.get_one(name)
+
+
+def create(creature: Creature) -> Creature:
+    return data.create(creature)
+
+
+def replace(creature: Creature) -> Creature:
+    return data.replace(creature)
+
+
+def modify(creature: Creature) -> Creature:
+    return data.modify(creature)
+
+
+def delete(name: str) -> bool:
+    return data.delete(name)
---
+++ learnfastapi/service/explorer.py
@@ -0,0 +1,26 @@
+from learnfastapi.model.explorer import Explorer
+import learnfastapi.fake.explorer as data
+
+
+def get_all() -> list[Explorer]:
+    return data.get_all()
+
+
+def get_one(name: str) -> Explorer | None:
+    return data.get_one(name)
+
+
+def create(explorer: Explorer) -> Explorer:
+    return data.create(explorer)
+
+
+def replace(explorer: Explorer) -> Explorer:
+    return data.replace(explorer)
+
+
+def modify(explorer: Explorer) -> Explorer:
+    return data.modify(explorer)
+
+
+def delete(name: str) -> bool:
+    return data.delete(name)
