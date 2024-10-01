1eb7c8bc23e80369b7707e320d303514a47f4e95
Foysal Ahamed <thisisfoysal@gmail.com>
2024-10-01 13:52:25 +0600

    Create root directory of db file if not exists

--- learnfastapi/data/init.py
+++ learnfastapi/data/init.py
@@ -15,6 +15,7 @@ def get_db(name: str | None = None, reset: bool = False):
         if not reset:
             return
         conn = None
+
     if not name:
         name = os.getenv("CRYPTID_SQLITE_DB")
         top_dir = Path(__file__).resolve().parents[1]  # repo top
@@ -22,6 +23,11 @@ def get_db(name: str | None = None, reset: bool = False):
         db_name = "cryptid.db"
         db_path = str(db_dir / db_name)
         name = os.getenv("CRYPTID_SQLITE_DB", db_path)
+
+    root_db_dir = Path(name).parent
+    if not root_db_dir.exists():
+        root_db_dir.mkdir(parents=True)
+
     conn = connect(name, check_same_thread=False)
     curs = conn.cursor()
 
