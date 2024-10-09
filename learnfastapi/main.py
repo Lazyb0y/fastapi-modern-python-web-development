4d02f8dc3cf0671f201d24593bc13766311612eb
Foysal Ahamed <thisisfoysal@gmail.com>
2024-10-09 17:14:29 +0600

    Return a big file with StreamingResponse

--- learnfastapi/main.py
+++ learnfastapi/main.py
@@ -1,5 +1,8 @@
+from typing import Generator
+
 from fastapi import FastAPI, File, UploadFile
-from fastapi.responses import FileResponse
+from fastapi.responses import FileResponse, StreamingResponse
+
 from learnfastapi.web import explorer, creature, user
 
 app = FastAPI()
@@ -7,6 +10,11 @@ app = FastAPI()
 app.include_router(explorer.router)
 app.include_router(creature.router)
 app.include_router(user.router)
+
+
+def gen_file(path: str) -> Generator:
+    with open(file=path, mode="rb") as file:
+        yield file.read()
 
 
 @app.post("/small")
@@ -22,6 +30,13 @@ async def upload_big_file(big_file: UploadFile) -> str:
 @app.get("/small/{name}")
 async def download_small_file(name) -> FileResponse:
     return FileResponse(name)
+
+
+@app.get("/big/{name}")
+async def download_big_file(name: str):
+    gen_expr = gen_file(path=name)
+    response = StreamingResponse(content=gen_expr, status_code=200)
+    return response
 
 
 if __name__ == "__main__":
