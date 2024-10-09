2d685bca50217ac693d5b420085132b25f6d6d0f
Foysal Ahamed <thisisfoysal@gmail.com>
2024-10-09 16:47:24 +0600

    Handle a small file upload with FastAPI

--- learnfastapi/main.py
+++ learnfastapi/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, File
 from learnfastapi.web import explorer, creature, user
 
 app = FastAPI()
@@ -6,6 +6,12 @@ app = FastAPI()
 app.include_router(explorer.router)
 app.include_router(creature.router)
 app.include_router(user.router)
+
+
+@app.post("/small")
+async def upload_small_file(small_file: bytes = File()):
+    return f"File size: {len(small_file)} bytes"
+
 
 if __name__ == "__main__":
     import uvicorn
