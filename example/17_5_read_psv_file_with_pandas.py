f29d61fa66dbc67af694ec57e6c6f611ff4fe09e
Foysal Ahamed <thisisfoysal@gmail.com>
2024-10-13 13:53:29 +0600

    Add example 17-5

---
+++ example/17_5_read_psv_file_with_pandas.py
@@ -0,0 +1,13 @@
+import sys
+
+import pandas
+
+
+def read_pandas(filename: str) -> pandas.DataFrame:
+    data = pandas.read_csv(filename, sep="|")
+    return data
+
+
+if __name__ == "__main__":
+    data = read_pandas(sys.argv[1])
+    print(data.head(5))
